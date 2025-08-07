a086775b77215fe9660aa7f092c7e94b10ef7e18
Abu-Hurairah-Rifat <147369255+Abu-Hurairah-Rifat@users.noreply.github.com>
2025-08-07 19:23:58 +0600

    permit Officer complete rifat

--- src/main/java/oop/section6/national_park_management/Permit_Officer/PermitOfficerReviewParkRegulationsAndPoliciesController.java
+++ src/main/java/oop/section6/national_park_management/Permit_Officer/PermitOfficerReviewParkRegulationsAndPoliciesController.java
@@ -1,4 +1,17 @@
 package oop.section6.national_park_management.Permit_Officer;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class PermitOfficerReviewParkRegulationsAndPoliciesController {
+
+    @FXML
+    private Label parkRegulationsAndPoliciesLabel;
+
+    @FXML
+    void backToDashboardButton(ActionEvent event) {
+
+    }
+
 }
