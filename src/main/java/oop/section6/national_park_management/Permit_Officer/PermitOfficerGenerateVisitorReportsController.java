55b0ecea8448747366507e33627767650d6ac41c
Abu-Hurairah-Rifat <147369255+Abu-Hurairah-Rifat@users.noreply.github.com>
2025-08-07 16:50:21 +0600

    7th commit r

---
+++ src/main/java/oop/section6/national_park_management/Permit_Officer/PermitOfficerGenerateVisitorReportsController.java
@@ -0,0 +1,62 @@
+package oop.section6.national_park_management.Permit_Officer;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class PermitOfficerGenerateVisitorReportsController {
+
+    @FXML
+    private TableColumn<?, ?> emailTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> noOfVisitorsTableColumn;
+
+    @FXML
+    private TableView<?> permitDataTableView;
+
+    @FXML
+    private TableColumn<?, ?> permitIdTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> permitTypeTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> phoneTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> totalFeeTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> visitDateTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> visitorNameTableColumn;
+
+    @FXML
+    void backToDashboardButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void generateDailyReportButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void generateReportButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void generateWeeklyReportButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewAllPermitsButton(ActionEvent event) {
+
+    }
+
+}
