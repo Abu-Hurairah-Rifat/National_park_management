4e224934203a855c17cf3fc8e3575d518d035419
Abu-Hurairah-Rifat <147369255+Abu-Hurairah-Rifat@users.noreply.github.com>
2025-08-07 19:52:50 +0600

    maintenance staff all classes and fxmls rifat

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,4 +7,6 @@ module oop.section6.national_park_management {
     exports oop.section6.national_park_management;
     exports oop.section6.national_park_management.Permit_Officer;
     opens oop.section6.national_park_management.Permit_Officer to javafx.fxml;
+    exports oop.section6.national_park_management.Maintenance_Staff;
+    opens oop.section6.national_park_management.Maintenance_Staff to javafx.fxml;
 }
---
+++ src/main/java/oop/section6/national_park_management/Maintenance_Staff/ReportNewMaintenanceIssueController.java
@@ -0,0 +1,4 @@
+package oop.section6.national_park_management.Maintenance_Staff;
+
+public class ReportNewMaintenanceIssueController {
+}
