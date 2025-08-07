9dcc5920f6b372a3afb4696b5bca7a53cbbe2376
Abu-Hurairah-Rifat <147369255+Abu-Hurairah-Rifat@users.noreply.github.com>
2025-08-07 17:45:09 +0600

    8th commit r

---
+++ src/main/java/oop/section6/national_park_management/Permit_Officer/PermitOfficerManageParkCapacityOrClosuresController.java
@@ -0,0 +1,45 @@
+package oop.section6.national_park_management.Permit_Officer;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class PermitOfficerManageParkCapacityOrClosuresController {
+
+    @FXML
+    private Label capacityErrorMessageLabel;
+
+    @FXML
+    private DatePicker closureEndDateDatePicker;
+
+    @FXML
+    private Label closureErrorMessageLabel;
+
+    @FXML
+    private TextArea closureReasonTextArea;
+
+    @FXML
+    private DatePicker closureStartDateDatePicker;
+
+    @FXML
+    private TextField maxCapacityTextField;
+
+    @FXML
+    void backToDashboardButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void schiduleClosureButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateCapacityButton(ActionEvent event) {
+
+    }
+
+}
