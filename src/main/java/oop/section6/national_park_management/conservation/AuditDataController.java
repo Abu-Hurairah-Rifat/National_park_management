10b7b5a894e9518f4b9cc429e52cf4c235bd0c7b
Ayesha-IUb <2211155@iub.edu.bd>
2025-08-16 12:18:33 +0600

    modal class of Maintenance Staff 1 rifat

--- src/main/java/oop/section6/national_park_management/conservation/AuditDataController.java
+++ src/main/java/oop/section6/national_park_management/conservation/AuditDataController.java
@@ -1,7 +1,12 @@
 package oop.section6.national_park_management.conservation;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+import oop.section6.national_park_management.HelloApplication;
 
 public class AuditDataController
 {
@@ -14,5 +19,16 @@ public class AuditDataController
 
     @javafx.fxml.FXML
     public void backToDashboard(ActionEvent actionEvent) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ConservatinDataAnalyst/Analyst_Audit_Data.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage nextStage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+            nextStage.setTitle("Lawachara National Park");
+            nextStage.setScene(scene);
+            nextStage.show();
+        }
+        catch (Exception e) {
+            //throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/oop/section6/national_park_management/conservation/ExportDatasetsController.java
+++ src/main/java/oop/section6/national_park_management/conservation/ExportDatasetsController.java
@@ -1,8 +1,13 @@
 package oop.section6.national_park_management.conservation;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+import oop.section6.national_park_management.HelloApplication;
 
 public class ExportDatasetsController
 {
@@ -19,6 +24,17 @@ public class ExportDatasetsController
 
     @javafx.fxml.FXML
     public void backToDashboard(ActionEvent actionEvent) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ConservatinDataAnalyst/Analyst_Export_Datasets.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage nextStage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+            nextStage.setTitle("Lawachara National Park");
+            nextStage.setScene(scene);
+            nextStage.show();
+        }
+        catch (Exception e) {
+            //throw new RuntimeException(e);
+        }
     }
 
     @javafx.fxml.FXML
