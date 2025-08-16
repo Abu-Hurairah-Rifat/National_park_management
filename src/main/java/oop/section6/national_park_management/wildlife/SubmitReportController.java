905d258684f9a10c67350041fb24bcdd0a17d850
Ayesha-IUb <2211155@iub.edu.bd>
2025-08-16 12:08:10 +0600

    modal class of Maintenance Staff 1 rifat

--- src/main/java/oop/section6/national_park_management/wildlife/SubmitReportController.java
+++ src/main/java/oop/section6/national_park_management/wildlife/SubmitReportController.java
@@ -1,8 +1,13 @@
 package oop.section6.national_park_management.wildlife;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import oop.section6.national_park_management.HelloApplication;
 
 public class SubmitReportController
 {
@@ -17,6 +22,17 @@ public class SubmitReportController
 
     @javafx.fxml.FXML
     public void backToDashboard(ActionEvent actionEvent) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("WildlifeResearcher/Researcher_Submit_Report.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage nextStage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+            nextStage.setTitle("Lawachara National Park");
+            nextStage.setScene(scene);
+            nextStage.show();
+        }
+        catch (Exception e) {
+            //throw new RuntimeException(e);
+        }
     }
 
     @javafx.fxml.FXML
--- src/main/java/oop/section6/national_park_management/wildlife/TrackEquipmentController.java
+++ src/main/java/oop/section6/national_park_management/wildlife/TrackEquipmentController.java
@@ -1,7 +1,12 @@
 package oop.section6.national_park_management.wildlife;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+import oop.section6.national_park_management.HelloApplication;
 
 public class TrackEquipmentController
 {
@@ -14,6 +19,17 @@ public class TrackEquipmentController
 
     @javafx.fxml.FXML
     public void backToDashboard(ActionEvent actionEvent) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("WildlifeResearcher/Researcher_Track_Equipment.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage nextStage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+            nextStage.setTitle("Lawachara National Park");
+            nextStage.setScene(scene);
+            nextStage.show();
+        }
+        catch (Exception e) {
+            //throw new RuntimeException(e);
+        }
 
     }
 }
