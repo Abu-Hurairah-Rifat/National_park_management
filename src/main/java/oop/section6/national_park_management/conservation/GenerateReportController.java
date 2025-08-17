49826bab3a616e2c6efeeddebcd538af6c7e0015
Ayesha-IUb <2211155@iub.edu.bd>
2025-08-17 15:53:07 +0600

    updating GenerateReportController.java

--- src/main/java/oop/section6/national_park_management/conservation/GenerateReportController.java
+++ src/main/java/oop/section6/national_park_management/conservation/GenerateReportController.java
@@ -8,9 +8,10 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import oop.section6.national_park_management.HelloApplication;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
-public class GenerateReportController
-{
+public class GenerateReportController {
     @javafx.fxml.FXML
     private TextArea contentArea;
     @javafx.fxml.FXML
@@ -24,7 +25,7 @@ public class GenerateReportController
 
     @javafx.fxml.FXML
     public void backToDashboard(ActionEvent actionEvent) {
-        try{
+        try {
             FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ConservatinDataAnalyst/Analyst_Generate_Report.fxml"));
             Scene scene = new Scene(fxmlLoader.load());
             Stage nextStage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
@@ -39,5 +40,17 @@ public class GenerateReportController
 
     @javafx.fxml.FXML
     public void generateReport(ActionEvent actionEvent) {
+        // Create and show information alert
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Report Status");
+        alert.setHeaderText("Report Generation Complete");
+        alert.setContentText("The report '" + titleField.getText() + "' has been successfully generated.");
+
+        alert.showAndWait();
+
+        // Optional: Clear the form after submission
+        titleField.clear();
+        contentArea.clear();
+        referencesArea.clear();
     }
 }
