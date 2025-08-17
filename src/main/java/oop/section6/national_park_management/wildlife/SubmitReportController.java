30aba40c97f9d922ff55890199fc7bb893ba8d36
Ayesha-IUb <2211155@iub.edu.bd>
2025-08-17 15:28:38 +0600

    updating SubmitReportController.java

--- src/main/java/oop/section6/national_park_management/wildlife/SubmitReportController.java
+++ src/main/java/oop/section6/national_park_management/wildlife/SubmitReportController.java
@@ -8,9 +8,10 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import oop.section6.national_park_management.HelloApplication;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
-public class SubmitReportController
-{
+public class SubmitReportController {
     @javafx.fxml.FXML
     private TextArea contentArea;
     @javafx.fxml.FXML
@@ -22,20 +23,29 @@ public class SubmitReportController
 
     @javafx.fxml.FXML
     public void backToDashboard(ActionEvent actionEvent) {
-        try{
+        try {
             FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("WildlifeResearcher/Researcher_Submit_Report.fxml"));
             Scene scene = new Scene(fxmlLoader.load());
-            Stage nextStage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+            Stage nextStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
             nextStage.setTitle("Lawachara National Park");
             nextStage.setScene(scene);
             nextStage.show();
-        }
-        catch (Exception e) {
-            //throw new RuntimeException(e);
+        } catch (Exception e) {
+
         }
     }
 
     @javafx.fxml.FXML
     public void submitReport(ActionEvent actionEvent) {
+        // Create and show information alert
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Report Submission");
+        alert.setHeaderText("Report Submitted Successfully");
+        alert.setContentText("Your report titled '" + titleField.getText() + "' has been submitted.");
+
+        alert.showAndWait();
+
+        titleField.clear();
+        contentArea.clear();
     }
 }
